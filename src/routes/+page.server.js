b5c79bd146fc16a4490889dd22f94576ffef9151
RicherBel <E@text>
2023-06-30 23:07:39 -0300

    Cambio en el query

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -2,7 +2,7 @@ import {Realizar_Query} from "$lib/server/mysql"
 
 export async function load(event) { //{fetch}
     async function GetAllImage() {       
-        let IdImageawait = await Realizar_Query(`select * from paints;`);
+        let IdImageawait = await Realizar_Query(`select * from paints ORDER BY id DESC;`); 
         // IdImageawait = []
         return IdImageawait;  
     };
