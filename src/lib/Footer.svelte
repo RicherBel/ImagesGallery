5e650e005dae2cb63d18665f396783a42e87e17e
RicherBel <85463050+RicherBel@users.noreply.github.com>
2023-08-28 00:31:12 -0300

    Arregle el url del footer

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,4 +1,4 @@
 <footer>
-    <div class="FraneImg"><img src="https://cdn.discordapp.com/attachments/1010640027350466724/1123295057747910829/image.png" alt="FooterImage"></div> 
+    <div class="FraneImg"><img src="https://upload.wikimedia.org/wikipedia/commons/7/72/Einhard.jpg" alt="FooterImage"></div> 
     <p>Made by No One</p>
 </footer>
